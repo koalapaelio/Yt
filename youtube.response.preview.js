6e3d6b6970a5dbe5492fac6b77116d132601c5c3
koalapaelio <koalapaelio@gmail.com>
2025-01-28 13:53:27 +0300

    Update youtube.response.preview.js

--- youtube.response.preview.js
+++ youtube.response.preview.js
@@ -1,4 +1,4 @@
-// https://raw.githubusercontent.com/Maasea/sgmodule/master/Script/Youtube/dist/youtube.response.preview.js
+// https://raw.githubusercontent.com/koalapaelio/Yt/refs/heads/main/youtube.response.preview.js
 // Build: 2024/11/24 11:47:49
 (() => {
   var qt = Object.defineProperty;
